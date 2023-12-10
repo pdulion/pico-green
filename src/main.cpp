87f88bdd4e6a2c8d695f36d022be485f3eaab145
Phillip DuLion <phillip@dulion.com>
2023-12-10 01:31:31 +0000

    Using SWD to install image

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "pico/cyw43_arch.h"
 
-int main(int, char**){
-    std::cout << "Hello, from green-pi!\n";
+using namespace std;
+
+int main(int, char**) {
+    stdio_init_all();
+
+    cout << "Hello, from green-pi!" << endl;
+    if (cyw43_arch_init()) {
+        cout << "Initialization Failed!" << endl;
+        return -1;
+    }
+
+    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }
 }
