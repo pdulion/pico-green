4322159c0f70fbe551470941b2da728335ebe7f4
Phillip DuLion <phillip@dulion.com>
2023-12-17 20:59:00 +0000

    500ms sleep

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,8 @@ int main(int, char **)
     {
         cout << "Blink!" << endl;
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(250);
+        sleep_ms(500);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(250);
+        sleep_ms(500);
     }
 }
