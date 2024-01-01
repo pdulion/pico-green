4399300f5747da561dcb93dee1e2d1f4b532d9cd
Phillip DuLion <phillip@dulion.com>
2024-01-01 03:07:16 +0000

    Adjust make lists

--- lwip_import.cmake
+++ lwip_import.cmake
@@ -8,6 +8,7 @@ target_sources(lwip_port PUBLIC ${PICO_SDK_PATH}/lib/lwip/src/apps/sntp/sntp.c)
 target_link_libraries(lwip_port PUBLIC
   freertos_port
   pico_lwip
+
   FreeRTOS-Kernel
   pico_cyw43_arch_lwip_sys_freertos
   pico_stdlib
